3fbce9a16add32e98f3ece04d6a6d57ebe1500ca
Edward Smith <edward.smith@mail.mcgill.ca>
2018-02-12 09:09:33 -0500

    updates to make work

--- scripts/models.py
+++ scripts/models.py
@@ -9,36 +9,36 @@ def upscale(faces, scope_name = 'up', is_train=True, reuse=False):
     with tf.variable_scope(scope_name, reuse=reuse) as vs:
         tl.layers.set_name_reuse(reuse)
 
-        face = InputLayer(faces, name='in/face')
-        upscale = Conv2d(face, 128, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init, name='n64s1/c_face')
+        face = InputLayer(faces, name='input')
+        upscale = Conv2d(face, 128, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init, name='cnn1')
     
         temp = upscale
         # residual blocks
         for i in range(16):
-            recall = Conv2d(upscale, 128, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init,  name='n64s1/c1/%s' % i)
-            recall = BatchNormLayer(recall, act=tf.nn.relu, is_train=is_train, gamma_init=g_init, name='n64s1/b1/%s' % i)
-            recall = Conv2d(recall, 128, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='n64s1/c2/%s' % i)
-            recall = BatchNormLayer(recall, is_train=is_train, gamma_init=g_init, name='n64s1/b2/%s' % i)
-            recall = ElementwiseLayer([upscale, recall], tf.add, 'b_residual_add/%s' % i)
+            recall = Conv2d(upscale, 128, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init,  name='res1/%s' % i)
+            recall = BatchNormLayer(recall, act=tf.nn.relu, is_train=is_train, gamma_init=g_init, name='res2/%s' % i)
+            recall = Conv2d(recall, 128, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='res3/%s' % i)
+            recall = BatchNormLayer(recall, is_train=is_train, gamma_init=g_init, name='res4/%s' % i)
+            recall = ElementwiseLayer([upscale, recall], tf.add, 'res5/%s' % i)
             upscale = recall
 
     
 
-        upscale = Conv2d(upscale, 128, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='n64s1/c/m')
-        upscale = BatchNormLayer(upscale, is_train=is_train, gamma_init=g_init, name='n64s1/b/m')
-        upscale = ElementwiseLayer([upscale, temp], tf.add, 'add3')
+        upscale = Conv2d(upscale, 128, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='cnn2')
+        upscale = BatchNormLayer(upscale, is_train=is_train, gamma_init=g_init, name='bn2')
+        upscale = ElementwiseLayer([upscale, temp], tf.add, 'sum2')
 
         # up-sampling
-        upscale = Conv2d(upscale, 256, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='n256s1/1')
-        upscale = SubpixelConv2d(upscale, scale=2, act=tf.nn.relu, name='pixelshufflerx2/1')
+        upscale = Conv2d(upscale, 256, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='cnn3')
+        upscale = SubpixelConv2d(upscale, scale=2, act=tf.nn.relu, name='subpixel3')
 
-        upscale = Conv2d(upscale, 128, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='n256s1/2')
-        upscale = SubpixelConv2d(upscale, scale=2, act=tf.nn.relu, name='pixelshufflerx2/2')
+        upscale = Conv2d(upscale, 128, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='cnn4')
+        upscale = SubpixelConv2d(upscale, scale=2, act=tf.nn.relu, name='subpixel4')
 
-        upscale = Conv2d(upscale, 64, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='n256s1/2ascasc')
-        upscale = SubpixelConv2d(upscale, scale=2, act=tf.nn.relu, name='pixelshufflerxcasc2/2')
+        upscale = Conv2d(upscale, 64, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='cnn5')
+        upscale = SubpixelConv2d(upscale, scale=2, act=tf.nn.relu, name='subpixel5')
 
-        upscale = Conv2d(upscale, 1, (1, 1), (1, 1), act=tf.nn.sigmoid, padding='SAME', W_init=w_init, name='out')
+        upscale = Conv2d(upscale, 1, (1, 1), (1, 1), act=tf.nn.sigmoid, padding='SAME', W_init=w_init, name='cnnout')
         return upscale, upscale.outputs
 
 
--- scripts/utils.py
+++ scripts/utils.py
@@ -17,12 +17,12 @@ def make_directories(checkpoint,savepoint):
 		os.makedirs(savepoint)
 
 
+# loads data for each files 
 def make_batch(files, h, l, valid = False, side = -1, occupancy = False):
-	
 	ratio = h//l
 	highs, lows, low_ups, sides =  [], [], [], []
 	start_time = time.time()
-	alter = (side == -1)  
+	alter = (side == -1)  ### if the odm side should be random or not 
 	for i,f in enumerate(files):
 		if alter: 
 			side = random.randint(0, 5) if not valid else i%6 
@@ -41,7 +41,7 @@ def make_batch(files, h, l, valid = False, side = -1, occupancy = False):
 
 			change = np.where(high == 0)
 			up[change] = 0
-			highs.append(high/float(h))
+			highs.append(high)
 			low_ups.append(up)
 
 		else: 
@@ -49,34 +49,43 @@ def make_batch(files, h, l, valid = False, side = -1, occupancy = False):
 			high[change] = 1. 
 			highs.append(high)
 
-		change = np.where(low == 0)
-		low[change] = l*2 
-		lows.append(low/float(l*2))
+		# change = np.where(low == 0)
+		# low[change] = l*2 
+		lows.append(low)
 		
 
 	gen = {'high':np.array(highs), 'low': np.array(lows),'low_up':np.array(low_ups), 'side': np.array(sides)}
 	return gen, start_time
    
+def make_objs(files): 
+	high_objs, low_objs = [], []
+	for f in files: 
+		file = f.split('_')[0][:-4] + 'full_object_'+ f.split('_')[-1]
+		face = sio.loadmat(file)
+		high_objs.append((face['model']))
+		low_objs.append((face['low_model']))
+	return np.array(high_objs), np.array(low_objs)
 
-
+# grabs all files for training 
 def grab_files(data_dir):
 	data_dir+='/'
 	files = [f for f in glob(data_dir + '*.mat') if 'face_' in f]
 	return files
 
-def save_networks(checkpoint_dir, sess, net, epoch, name = None ):
+# saves netowrks during training 
+def save_networks(checkpoint_dir, sess, net, epoch, name = '' ):
 	print("[*] Saving checkpoints...")
 	if not os.path.exists(checkpoint_dir):
 		os.makedirs(checkpoint_dir)
-	epoch = '_' + epoch
-	if name is not None: epoch = name + epoch
-	else: epoch = 'net'	+ epoch
-	epoch = epoch + '.npz'
+	epoch = name + '_' + epoch + '.npz'
+	best = name + '_best.npz'
 	net_name = os.path.join(checkpoint_dir, epoch)
+	best_name = os.path.join(checkpoint_dir, best)
 	tl.files.save_npz(net.all_params, name=net_name, sess=sess)
+	tl.files.save_npz(net.all_params, name=best_name, sess=sess)
 	print("[*] Saving checkpoints SUCCESS!")
 
-
+# averages over array of numbers for smooth graphing
 def savitzky_golay(y, window_size, order, deriv=0, rate=1):
 	import numpy as np
 	from math import factorial
@@ -98,13 +107,10 @@ def savitzky_golay(y, window_size, order, deriv=0, rate=1):
 	y = np.concatenate((firstvals, y, lastvals))
 	return np.convolve( m[::-1], y, mode='valid')
 
-
-def load_networks(checkpoint_dir, sess, net, epoch, name = None): 
+# load networks 
+def load_networks(checkpoint_dir, sess, net, epoch, name = ''): 
 	print "[*] Loading checkpoints..." 
-	epoch = '_' + epoch
-	if name is not None: epoch = name + epoch
-	else: epoch = 'net'	+ epoch
-	epoch = epoch + '.npz'
+	epoch = name + '_' + epoch + '.npz'
 	net_name = os.path.join(checkpoint_dir,  epoch)
 	
 	if not (os.path.exists(net_name)):
@@ -115,12 +121,16 @@ def load_networks(checkpoint_dir, sess, net, epoch, name = None):
 		tl.files.assign_params(sess, net_loaded_params, net)
 		print "[*] Loading checkpoint SUCCESS!"
 		
-			
+# plots and saves graphs 		
 def render_graphs(save_dir,epoch, recon_loss, valid_loss, exact_valid_loss, name = 'depth'): 
 	if len(recon_loss)>101: 
 		valid_list = []
 		for i in range(epoch+1): 
 			valid_list.append((i+1)*len(recon_loss)//(epoch+1) )
+
+		recon_loss = recon_loss[len(recon_loss)//2:]
+		valid_list, valid_loss = valid_list[len(valid_list)//2:],valid_loss[len(valid_loss)//2:]
+
 		smoothed_recon = savitzky_golay(recon_loss, 101, 3)
 		plt.plot(recon_loss, color='blue') 
 		plt.plot(smoothed_recon, color = 'red')
@@ -129,15 +139,131 @@ def render_graphs(save_dir,epoch, recon_loss, valid_loss, exact_valid_loss, name
 		plt.savefig(save_dir+'/' + name + '_' + 'recon.png' )
 		plt.clf()
 	if len(exact_valid_loss)>5: 
-	
 		plt.plot( exact_valid_loss[5:])
 		plt.grid()
 		plt.savefig(save_dir+'/' + name + '_' + 'exact_recon.png' )
 		plt.clf()
 
-def evaluate_voxel_prediction(prediction,gt):
-  """  The prediction and gt are 3 dim voxels. Each voxel has values 1 or 0"""
-  intersection = np.sum(np.logical_and(prediction,gt))
-  union = np.sum(np.logical_or(prediction,gt))
-  IoU = intersection / union
-  return IoU
+
+
+# comptue complete depth map from ranged prediction and upsampled low res image
+def recover_depths(preds, ups, high, dis): 
+	preds =  np.round_(preds*dis).reshape((-1,high,high)) 
+	ups = np.array(ups).reshape((-1,high,high))
+
+	for pred, up, i in zip(preds, ups, range(preds.shape[0])):
+		pred = np.array(pred)
+		pred  = up + pred # add to upsampled low resolution odm
+		off = np.where(pred > high)  # set values which predict to high to be unoccupited -> 0        
+		pred[off] = 0. 
+		preds[i] = pred
+	return preds
+
+def recover_occupancy(preds, high, threshold = .5): 
+	preds[np.where( preds >  threshold)] = 1.
+	preds[np.where( preds <= threshold)] = 0.
+	return preds.reshape((-1,high,high))
+
+
+
+
+
+
+# reconvers full odm from occupancy and depth map
+def recover_odm(depths, occs, ups, high, low, dis, side, threshold = 20):
+
+	# recover exact occupancy and depths information and combine to creat the odms
+	odms = recover_depths(depths, ups, high, dis)
+	occs = recover_occupancy(occs, high)
+	off = np.where(occs == 0 )
+	odms[off] = 0
+
+	# smoothing
+	for i,odm in enumerate(odms):
+		copy = np.array(odm)
+		on = np.where(odm != 0)
+		for x,y in zip(*on):
+			window = odm[max(0,x-3):min(high-1, x+4),max(0,y-3):min(high-1,y+4)] #window
+		
+			considered =  np.where( window != 0)
+			total = 0.
+			count = 0.
+			for a,b in zip(*considered): 
+				if abs(window[a,b] -odm[x,y]) <threshold: 
+					total += window[a,b]
+					count +=1.
+			
+			if count > odm[x,y]:
+				copy[x,y] = total/count
+		odms[i] = np.round_(copy)
+
+
+	return odms
+
+# nearest neighbor upsampling of object
+def upsample(obj, high, low): 
+	ratio = high // low 
+	big_obj = np.zeros((high,high,high))
+	for i in range(low): 
+		for j in range(low): 
+			for k in range(low):
+				big_obj[i*ratio: (i+1)*ratio, j*ratio:(j+1)*ratio, k*ratio:(k+1)*ratio] = obj[i,j,k]
+	return big_obj
+
+
+def apply_occupancy(obj, odms): 
+	prediction = np.zeros(obj.shape)
+	unoccupied = np.where(odms==0) 
+	for x,y,z in zip(*unoccupied): 
+		if x == 0 or x == 1: 
+			prediction[y,z,:]-=0.25
+		elif x == 2 or x == 3: 
+			prediction[y,:,z]-=0.25
+		else: 
+			prediction[:,y,z]-=0.25
+	ones = np.where(prediction>.6)
+	zeros = np.where(prediction<.6)
+	prediction[ones] = 1 
+	prediction[zeros] = 0 
+	return prediction 
+
+def apply_depth(obj, high, odms):
+	# recovering the origional data collected 
+	off = np.where(odms == 0)
+	for i in range(6): 
+		face = np.array(odms[i])
+		on  = np.where(face != 0)
+		if i%2 == 0: 
+			face[on] = 256 - face[on] + 2  # matches how gt obj is created 
+		else:
+			face[on] = face[on]  -1 
+		print odms.shape, face.shape
+		odms[i] = face 
+	odms[off] = high * 2 
+
+	prediction = np.array(obj)
+	depths = np.where(odms<=high)
+	for x,y,z in zip(*depths):
+		pos = odms[x,y,z]
+		if x == 0: 
+				data[y,z,pos:high]-=.25
+		if x == 1:
+				data[y,z,0:pos]-=.25 
+		if x == 2: 
+			 	data[y,pos:high,z]-=.25
+		elif x == 3: 
+				data[y,0:pos,z]-=.25
+		elif x == 4: 
+				data[pos:high,y,z]-=.25
+		elif x == 5: 
+				data[0:pos,y, z]-=.25
+				5
+	ones = np.where(prediction>=1)
+	zeros = np.where(prediction<1)
+	prediction[ones] = 1 
+	prediction[zeros] = 0 
+	return prediction 
+
+
+def produce( prediction, obj, small_obj):
+	return
