8cf8ad9b3ca0ba0af5a63064b3b5022a9229d842
Edward Smith <edward.smith@mail.mcgill.ca>
2018-06-04 08:43:33 -0400

    updated voxel2mesh function to make more efficient
    updated the voxel2mesh script to make only exposed faces visible. This uses far less faces and vertices, and makes graph operations such and decimation and smoothing possible.

--- scripts/voxel.py
+++ scripts/voxel.py
@@ -3,35 +3,91 @@ import os
 def call(command):
     os.system('%s > /dev/null 2>&1' % command)
 
+# this method creates a mesh copy of a voxel model
+# it is efficinet as only exposed faces are drawn
+#`could be written a tad better, but I think this way is the easiest to read 
 def voxel2mesh(voxels, threshold=0):
-    cube_verts = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 0],
-                  [1, 1, 1]]  # 8 points
+    # these faces and verticies define the various sides of a cube
+    top_verts = [[0,0,1], [1,0,1], [1,1,1], [0,1,1]]    
+    top_faces = [[1,2,4], [2,3,4]] 
 
-    cube_faces = [[0, 1, 2], [1, 3, 2], [2, 3, 6], [3, 7, 6], [0, 2, 6], [0, 6, 4], [0, 5, 1],
-                  [0, 4, 5], [6, 7, 5], [6, 5, 4], [1, 7, 3], [1, 5, 7]]  # 12 face
+    bottom_verts = [[0,0,0], [1,0,0], [1,1,0], [0,1,0]]    
+    bottom_faces = [[2,1,4], [3,2,4]] 
 
-    cube_verts = np.array(cube_verts)
-    cube_faces = np.array(cube_faces) + 1
+    left_verts = [[0,0,0], [0,0,1], [0,1,0], [0,1,1]]
+    left_faces = [[1,2,4], [3,1,4]]
+
+    right_verts = [[1,0,0], [1,0,1], [1,1,0], [1,1,1]]
+    right_faces = [[2,1,4], [1,3,4]]
+
+    front_verts = [[0,1,0], [1,1,0], [0,1,1], [1,1,1]]
+    front_faces = [[2,1,4], [1,3,4]]
+
+    back_verts = [[0,0,0], [1,0,0], [0,0,1], [1,0,1]]
+    back_faces = [[1,2,4], [3,1,4]]
+
+    top_verts = np.array(top_verts)
+    top_faces = np.array(top_faces)
+    bottom_verts = np.array(bottom_verts)
+    bottom_faces = np.array(bottom_faces)
+    left_verts = np.array(left_verts)
+    left_faces = np.array(left_faces)
+    right_verts = np.array(right_verts)
+    right_faces = np.array(right_faces)
+    front_verts = np.array(front_verts)
+    front_faces = np.array(front_faces)
+    back_verts = np.array(back_verts)
+    back_faces = np.array(back_faces)
+
+    dim = voxels.shape[0]
+    new_voxels = np.zeros((dim+2, dim+2, dim+2))
+    new_voxels[1:dim+1,1:dim+1,1:dim+1 ] = voxels
+    voxels= new_voxels
+
 
     scale = 0.01
-    cube_dist_scale = 1.1
+    cube_dist_scale = 1.
     verts = []
     faces = []
     curr_vert = 0
     a,b,c= np.where(voxels>threshold)
     for i,j,k in zip(a,b,c):
-        try: 
-            if voxels[i+1,j,k]<threshold or voxels[i-1,j,k]<threshold  or voxels[i,j+1,k]<threshold  or voxels[i,j-1,k]<threshold or voxels[i,j,k+1]<threshold or voxels[i,j,k-1]<threshold:
-                verts.extend(scale * (cube_verts + cube_dist_scale * np.array([[i, j, k]])))
-                faces.extend(cube_faces + curr_vert)
-                curr_vert += len(cube_verts)
-        except IndexError:
-            verts.extend(scale * (cube_verts + cube_dist_scale * np.array([[i, j, k]])))
-            faces.extend(cube_faces + curr_vert)
-            curr_vert += len(cube_verts)
-        
+        #top
+        if voxels[i,j,k+1]<threshold: 
+            verts.extend(scale * (top_verts + cube_dist_scale * np.array([[i-1, j-1, k-1]])))
+            faces.extend(top_faces + curr_vert)
+            curr_vert += len(top_verts)
     
-
+        #bottom
+        if voxels[i,j,k-1]<threshold: 
+            verts.extend(scale * (bottom_verts + cube_dist_scale * np.array([[i-1, j-1, k-1]])))
+            faces.extend(bottom_faces + curr_vert)
+            curr_vert += len(bottom_verts)
+            
+        #left
+        if voxels[i-1,j,k]<threshold: 
+            verts.extend(scale * (left_verts+ cube_dist_scale * np.array([[i-1, j-1, k-1]])))
+            faces.extend(left_faces + curr_vert)
+            curr_vert += len(left_verts)
+            
+        #right
+        if voxels[i+1,j,k]<threshold: 
+            verts.extend(scale * (right_verts + cube_dist_scale * np.array([[i-1, j-1, k-1]])))
+            faces.extend(right_faces + curr_vert)
+            curr_vert += len(right_verts)
+            
+        #front
+        if voxels[i,j+1,k]<threshold: 
+            verts.extend(scale * (front_verts + cube_dist_scale * np.array([[i-1, j-1, k-1]])))
+            faces.extend(front_faces + curr_vert)
+            curr_vert += len(front_verts)
+            
+        #back
+        if voxels[i,j-1,k]<threshold: 
+            verts.extend(scale * (back_verts + cube_dist_scale * np.array([[i-1, j-1, k-1]])))
+            faces.extend(back_faces + curr_vert)
+            curr_vert += len(back_verts)
+            
     return np.array(verts), np.array(faces)
 
 
